28e2ae09518735330ef229216237f2b9089cb005
MohDid <Piranesis@gmail.com>
2017-03-27 00:11:10 +0200

    Create Note.php

---
+++ notesBundle/Entity/Note.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace notesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Note
+ *
+ * @ORM\Table(name="note")
+ * @ORM\Entity(repositoryClass="notesBundle\Repository\NoteRepository")
+ */
+class Note
+{
+    /**
+    * @ORM\ManyToOne(targetEntity="notesBundle\Entity\Category")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $category;
+
+    public function setCategory(Category $category)
+    {
+      $this->category = $category;
+      return $this;
+    }
+
+    public function getCategory()
+    {
+      return $this->category;
+    }
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    // /**
+    //  * @var int
+    //  *
+    //  * @ORM\Column(name="id_category", type="integer")
+    //  */
+    // private $idCategory;
+
+    public function __contruct()
+    {
+      $this->date = new \Datetime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Note
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Note
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Note
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
